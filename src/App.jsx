d211551a4ce418ce890a4fefe0327f9ea0db6600
SeanCallinanWebDev <sean.callinan@gmail.com>
2019-04-26 19:05:32 +0930

    added fake server data to title and aggregate counters

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-let defaultTextColor = "#fff";
-let defaultPadding = "0 20px";
-let defaultDisplay = "inline-block";
 let defaultStyle = {
-    color: defaultTextColor,
-    padding: defaultPadding,
-    display: defaultDisplay
+    color: "#fff",
+    padding: "0 20px",
+    display: "inline-block"
 };
 
-function Aggregate() {
+let fakeServerData = {
+    user: {
+        name: "Sean",
+        playlists: [
+            {
+                name: "My Favourites",
+                image: "https://via.placeholder.com/150",
+                songs: [
+                    { name: "beat it", duration: 10000 },
+                    { name: "cannelloni macaroni", duration: 11000 },
+                    { name: "rosa helikopter", duration: 12000 }
+                ]
+            },
+            {
+                name: "Another Playlist",
+                image: "https://via.placeholder.com/150",
+                songs: [
+                    { name: "le song", duration: 13000 },
+                    { name: "the song", duration: 44000 },
+                    { name: "sangen", duration: 12000 }
+                ]
+            },
+            {
+                name: "Yet more",
+                image: "https://via.placeholder.com/150",
+                songs: [
+                    { name: "le song", duration: 13000 },
+                    { name: "the song", duration: 44000 },
+                    { name: "sangen", duration: 12000 }
+                ]
+            },
+            {
+                name: "Wow",
+                image: "https://via.placeholder.com/150",
+                songs: [
+                    { name: "le song", duration: 13000 },
+                    { name: "the song", duration: 44000 },
+                    { name: "sangen", duration: 12000 }
+                ]
+            }
+        ]
+    }
+};
+
+function Aggregate(props) {
     return (
         <div style={defaultStyle}>
-            <h3 style={defaultStyle}>Number and Text</h3>
+            <h3 style={defaultStyle}>
+                {props && props.count} {props && props.text}
+            </h3>
         </div>
     );
 }
@@ -31,7 +74,7 @@ function Playlist() {
         <div style={defaultStyle}>
             <img src="https://via.placeholder.com/150" alt="placeholder" />
             <h3>Playlist Name</h3>
-            <ul style={{ "text-align": "left" }}>
+            <ul>
                 <li>Song 1</li>
                 <li>Song 1</li>
                 <li>Song 1</li>
@@ -41,22 +84,58 @@ function Playlist() {
 }
 
 function App() {
+    const [serverData, setServerData] = useState(null);
+
+    useEffect(() => {
+        console.log("componentDidMount");
+        setTimeout(() => {
+            setServerData(fakeServerData);
+        }, 2000);
+    }, []);
+
+    function getTotalDuration() {
+        let allSongs = serverData.user.playlists.reduce(
+            (songs, eachPlaylist) => {
+                return songs.concat(eachPlaylist.songs);
+            },
+            []
+        );
+
+        let totalDuration = allSongs.reduce((sum, eachSong) => {
+            return sum + eachSong.duration;
+        }, 0);
+
+        return Math.round(totalDuration / 60 / 60);
+    }
+
     return (
         <div className="App">
-            <header className="App-header">
-                <h1 style={{ ...defaultStyle, margin: "0" }}>Title</h1>
-                <div>
-                    <Aggregate />
-                    <Aggregate />
-                </div>
-                <Filter />
-                <div>
-                    <Playlist />
-                    <Playlist />
-                    <Playlist />
-                    <Playlist />
-                </div>
-            </header>
+            {serverData ? (
+                <header className="App-header">
+                    <h1 style={{ ...defaultStyle, margin: "0" }}>
+                        {serverData.user.name}'s Playlists
+                    </h1>
+                    <div>
+                        <Aggregate
+                            count={serverData.user.playlists.length}
+                            text="playlists"
+                        />
+                        <Aggregate count={getTotalDuration()} text="hours" />
+                    </div>
+
+                    <Filter />
+                    <div>
+                        <Playlist />
+                        <Playlist />
+                        <Playlist />
+                        <Playlist />
+                    </div>
+                </header>
+            ) : (
+                <header className="App-header">
+                    <p style={{ ...defaultStyle, margin: "0" }}>loading...</p>
+                </header>
+            )}
         </div>
     );
 }
